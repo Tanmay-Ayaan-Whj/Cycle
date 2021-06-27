3e53cbee089de6b29fce2493dd9d31faad11a70a
Tanmay-Ayaan-Whj <80614278+Tanmay-Ayaan-Whj@users.noreply.github.com>
2021-06-27 10:14:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,81 @@
+var path,mainCyclist,mainCyclist2;
+var pathImg,mainRacerImg1,mainRacerImg2;
+var bellSound;
+
+
+var END =0;
+var PLAY =1;
+var gameState = PLAY;
+
+var distance=0;
+
+function preload(){
+  pathImg = loadImage("images/Road.png");
+  mainRacerImg1 = loadAnimation("images/mainPlayer1.png","images/mainPlayer2.png");
+  mainRacerImg2= loadAnimation("images/mainPlayer3.png");
+  
+  
+}
+
+
+
+function setup(){
+  
+  
+createCanvas(500,300);
+  
+// Moving background
+path=createSprite(100,150);
+path.addImage(pathImg);
+path.velocityX = -5;
+
+//creating boy running
+mainCyclist  = createSprite(70,150,20,20);
+mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
+mainCyclist.scale=0.07;
+  
+  
+mainCyclist2  = createSprite(100,150,20,20);
+mainCyclist2.addAnimation('SahilRunning',mainRacerImg1);
+mainCyclist2.scale=0.07;
+}
+
+function draw() {
+  background(0);
+  
+  drawSprites();
+  textSize(20);
+  fill(255);
+  text("Distance: "+ distance,350,30);
+
+  
+  
+  if(gameState===PLAY){
+  
+   mainCyclist.y = World.mouseY;
+    
+    if(frameCount<2000){
+      distance=distance+2;
+      
+      
+    
+      }
+      
+      
+      
+      }
+      
+      
+    
+  
+   edges= createEdgeSprites();
+   mainCyclist .collide(edges);
+   
+  
+  //code to reset the background
+  if(path.x < 0 ){
+    path.x = width/2;
+  }
+    
+ 
+}
